00227b57cf27aa95d01e31e5e096c9851395f6dd
NilminiDharmakeerthi94 <nilmini.dharmakeerthi@gmail.com>
2018-11-10 00:23:25 +0530

    min,max functions PHP

---
+++ min_maxfunction.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo (max(array('3','5','9','1')));    //get max value in array used max() function
+echo "<br>";       
+echo (min(array('2','5','2')))   //get minimum value in array used min() function
+
+ ?>
